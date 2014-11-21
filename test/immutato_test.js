b8f4175c956c6fb5b38a7a2d791a004044bb0212
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-11-21 19:41:28 +0100

    added test. reached 100% coverage

--- test/immutato_test.js
+++ test/immutato_test.js
@@ -21,6 +21,151 @@ describe('immutato', function() {
     });
 
 
+    it('with default configuration', function() {
+        immutato.CACHE_COUNT.should.be.equal(300);
+    });
+
+    it('has a configure method', function() {
+        immutato.configure.should.be.a('function');
+    });
+
+    describe('configure method', function() {
+        var reconfigured;
+        
+        before(function() {
+            reconfigured = immutato.configure(2);
+        });
+
+        it('return a new shaper function', function() {
+            reconfigured.should.be.a('function');
+        });
+
+        it('has changed configuration', function() {
+            reconfigured.CACHE_COUNT.should.be.equal(2);
+        });
+    });
+
+    describe('TEST_HOOKS is filled with test internal', function() {
+        var reconfigured;
+        var TEST_HOOKS = {};
+        var makeAnimal;
+        
+        before(function() {
+            reconfigured = immutato.configure(2,TEST_HOOKS);
+            makeAnimal = reconfigured({
+                name: null,
+                kind: null,
+                color: null
+            }, 'Animal');
+        });
+
+        describe('transactionsForClasses',function(){
+            it('contains transactionMaps for shapes', function() {
+                TEST_HOOKS.transactionsForClasses.should.be.a('object');
+            });
+
+            it('transactionMaps are hooked at shape creation time', function() {
+                TEST_HOOKS.transactionsForClasses.Animal.should.be.an('array');
+            });
+
+
+    
+        });
+        
+    });
+
+    describe('transactionMaps', function() {
+        var reconfigured;
+        var TEST_HOOKS = {};
+        var makeAnimal;
+        var transactionMap;
+        
+        before(function() {
+            reconfigured = immutato.configure(2,TEST_HOOKS);
+            makeAnimal = reconfigured({
+                name: null,
+                kind: null,
+                color: null
+            }, 'Animal');
+            transactionMap = TEST_HOOKS.transactionsForClasses.Animal;
+        });
+
+        it('is empty at shape created', function() {
+            
+            Object.keys(transactionMap).length.should.be.equal(0);
+        });
+
+
+        it('each new instance create a transactionMap', function() {
+            var animal = makeAnimal({
+                name: 'Fido',
+                kind: 'dog',
+                color: 'black'
+            });
+            transactionMap.length.should.be.equal(1);
+            animal.dispose();
+        });
+
+
+        it('disposing instances of a class set transactionMap to null', function() {
+            var animal = makeAnimal({
+                name: 'Fido',
+                kind: 'dog',
+                color: 'black'
+            });
+            var another = makeAnimal({
+                name: 'Kitty',
+                kind: 'cat',
+                color: 'white'
+            });
+            var len = Object.keys(transactionMap).length;
+            animal.dispose();
+            Object.keys(transactionMap).length.should.be.equal(len);
+            expect(transactionMap[0]).to.be.equal(null);
+            
+        });
+
+
+        
+
+
+        
+
+    
+        
+    });
+
+
+    describe('CACHE_COUNT define amount of first version instance property indexes to cache at start', function() {
+        var reconfigured;
+        
+        before(function() {
+            reconfigured = immutato.configure(2);
+        });
+
+        it('build first property indexes at runtime if not caches', function() {
+            var makeAnimal = reconfigured({
+                name: null,
+                kind: null,
+                color: null
+            }, 'Animal');
+
+            var animal = makeAnimal({
+                name: 'Fido',
+                kind: 'dog',
+                color: 'black'
+            });
+
+            animal.name.should.be.a('function');
+            animal.kind.should.be.a('function');
+            animal.color.should.be.a('function');
+            animal.name().should.be.equal('Fido');
+            animal.kind().should.be.equal('dog');
+            animal.color().should.be.equal('black');
+            animal.dispose();
+        });
+    });
+
     describe('that return a factory', function() {
         var makePerson;
 
@@ -41,6 +186,10 @@ describe('immutato', function() {
         describe('that build an instance', function() {
             var person;
 
+            after(function(){
+                person.dispose();
+            });
+            
             before(function() {
                 person = makePerson({
                     name: 'Andrea',
